8b2df57c12221539f27dcc3c213f11bcea76bd86
James Brown <jbrown@nebraskon.com>
2022-06-25 09:53:29 -0500

    adding module_5 folder

---
+++ module_5/mongodb_test.py
@@ -0,0 +1,7 @@
+from pymongo import MongoClient
+
+url = "mongodb+srv://admin:admin@cluster0.mbap3.mongodb.net/?retryWrites=true&w=majority"
+client = MongoClient(url)
+db = client.pytech
+print("Pytech Collection List")
+print(db.list_collection_names())
